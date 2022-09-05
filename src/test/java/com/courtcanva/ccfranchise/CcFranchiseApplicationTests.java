8c2fad3f6baa1a302be2c8832c63062ab1bd8880
jiangcan <can774899203@gmail.com>
2022-09-05 22:00:23 +1000

    first commit

---
+++ src/test/java/com/courtcanva/ccfranchise/CcFranchiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courtcanva.ccfranchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcFranchiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
