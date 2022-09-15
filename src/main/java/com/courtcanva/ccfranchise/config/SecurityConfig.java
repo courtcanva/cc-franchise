983217d88b58ad693fbf540d6e7cc291f2effc90
leonandhard <ruoxi.bao96@gmail.com>
2022-09-16 01:39:06 +1000

    add config

--- src/main/java/com/courtcanva/ccfranchise/config/SecurityConfig.java
+++ src/main/java/com/courtcanva/ccfranchise/config/SecurityConfig.java
@@ -1,2 +1,21 @@
-package com.courtcanva.ccfranchise.config;public class SecurityConfig {
+package com.courtcanva.ccfranchise.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests().antMatchers("/*").permitAll();
+        return http.build();
+    }
 }
