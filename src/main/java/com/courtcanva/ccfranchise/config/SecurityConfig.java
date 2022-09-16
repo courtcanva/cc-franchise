6239bf4012b53694959af1df57c8107974fe0c67
leonandhard <ruoxi.bao96@gmail.com>
2022-09-16 13:11:50 +1000

    feature: add csrf cors config

--- src/main/java/com/courtcanva/ccfranchise/config/SecurityConfig.java
+++ src/main/java/com/courtcanva/ccfranchise/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.courtcanva.ccfranchise.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
+@ConfigurationProperties(prefix = "management.endpoints.web.cors")
 public class SecurityConfig {
+    @Value("${allowed-origins}")
+    private List<String> origins;
+
+    @Value("${allowed-methods}")
+    private List<String> methods;
+
+    @Value("${allowed-headers}")
+    private List<String> heads;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
+                .cors().configurationSource(request -> {
+                    var cors = new CorsConfiguration();
+                    cors.setAllowedMethods(methods);
+                    cors.setAllowedOrigins(origins);
+                    cors.setAllowedHeaders(heads);
+                    return cors;
+                })
+                .and()
                 .authorizeHttpRequests().antMatchers("/*").permitAll();
         return http.build();
     }
 }
+
