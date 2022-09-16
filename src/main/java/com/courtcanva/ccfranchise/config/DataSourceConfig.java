246b3e89f404d97d9ec8601863c1d4b204a80feb
Jiayu Yuan <jiayuu0531@outlook.com>
2022-09-16 20:58:38 +1000

    db migration

---
+++ src/main/java/com/courtcanva/ccfranchise/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.courtcanva.ccfranchise.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
