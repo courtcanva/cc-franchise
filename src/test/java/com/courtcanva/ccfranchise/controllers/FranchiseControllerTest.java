6e7977992fe74ec86fe71493a86dface6608c668
leonandhard <ruoxi.bao96@gmail.com>
2022-09-17 17:58:39 +1000

    add staff service test

---
+++ src/test/java/com/courtcanva/ccfranchise/controllers/FranchiseControllerTest.java
@@ -0,0 +1,12 @@
+package com.courtcanva.ccfranchise.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class FranchiseControllerTest {
+
+    
+
+}
