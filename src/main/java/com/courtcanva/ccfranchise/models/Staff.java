7e3f428e1062f4eac45030ff259359a520dbeee7
Prom1s1ngYoung <yqin7516@gmail.com>
2022-09-26 17:11:13 +1000

    Format modify and remove unused reference

--- src/main/java/com/courtcanva/ccfranchise/models/Staff.java
+++ src/main/java/com/courtcanva/ccfranchise/models/Staff.java
@@ -21,7 +21,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.Constraint;
 import java.time.OffsetDateTime;
 
 
--- src/main/java/com/courtcanva/ccfranchise/repositories/FranchiseeRepository.java
+++ src/main/java/com/courtcanva/ccfranchise/repositories/FranchiseeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FranchiseeRepository extends JpaRepository<Franchisee, Long> {
 
     boolean existsFranchiseeByAbn(String abn);
+
 }
