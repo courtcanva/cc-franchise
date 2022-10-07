0069c9405603f2f83236849b3d12757b6b386ac2
Kerry Yuan <jiayuu0531@outlook.com>
2022-10-08 10:26:31 +1100

    cc24 - split one TestHelper file into four separate TestHelper files

---
+++ src/test/java/com/courtcanva/ccfranchise/utils/StaffTestHelper.java
@@ -0,0 +1,47 @@
+package com.courtcanva.ccfranchise.utils;
+
+import com.courtcanva.ccfranchise.constants.AUState;
+import com.courtcanva.ccfranchise.dtos.StaffGetDto;
+import com.courtcanva.ccfranchise.dtos.StaffPostDto;
+import com.courtcanva.ccfranchise.models.Staff;
+
+public class StaffTestHelper {
+    public static Staff createStaff() {
+
+        return Staff.builder()
+                .id(1232L)
+                .isVerified(false)
+                .firstName("ll")
+                .lastName("ff")
+                .email("666@gmail.com")
+                .residentialAddress("fsdfsdafa")
+                .build();
+    }
+
+    public static StaffGetDto createStaffGetDto() {
+
+        return StaffGetDto.builder()
+                .staffId(1232L)
+                .isVerified(false)
+                .firstName("ll")
+                .lastName("ff")
+                .email("666@gmail.com")
+                .residentialAddress("fsdfsdafa")
+                .build();
+    }
+
+    public static StaffPostDto createStaffPostDto() {
+
+        return StaffPostDto.builder()
+                .residentialAddress("gadsfadsfdsa")
+                .email("baoruoxi@163.com")
+                .state(AUState.ACT)
+                .postcode(3213)
+                .password("Bfasdf1123213")
+                .phoneNumber("0466277688")
+                .firstName("first")
+                .lastName("last")
+                .build();
+    }
+
+}
