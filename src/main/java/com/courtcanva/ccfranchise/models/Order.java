7f7ae6079244e817fa4f92f3c5361659a5f6a55a
Iris99999 <753891368@qq.com>
2022-10-25 18:53:02 +0800

    create order entity and order status

---
+++ src/main/java/com/courtcanva/ccfranchise/models/Order.java
@@ -0,0 +1,68 @@
+package com.courtcanva.ccfranchise.models;
+
+import com.courtcanva.ccfranchise.constants.OrderStatus;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "`order`")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String contactInformation;
+
+    @Column(nullable = false)
+    private String designInformation;
+
+    @Column(nullable = false)
+    private String postcode;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    private BigDecimal paidAmount;
+
+    @Column(nullable = false)
+    private BigDecimal unpaidAmount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private OrderStatus status= OrderStatus.UNASSIGNED;
+
+    @Column(nullable = false, updatable = false, name = "created_at")
+    @CreationTimestamp
+    private OffsetDateTime createdTime;
+
+    @Column(nullable = false, name = "updated_at")
+    @UpdateTimestamp
+    private OffsetDateTime updatedTime;
+
+    @JoinColumn(name = "franchisee_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Franchisee franchisee;
+
+    @Column
+    private String invoiceLink;
+}
