b1d7b2a127f19d2a3b8eecf02bfdb2b8d00bf85b
Iris99999 <753891368@qq.com>
2022-10-26 21:22:03 +0800

    change order status

--- src/main/java/com/courtcanva/ccfranchise/constants/OrderStatus.java
+++ src/main/java/com/courtcanva/ccfranchise/constants/OrderStatus.java
@@ -1,20 +1,9 @@
 package com.courtcanva.ccfranchise.constants;
 
 public enum OrderStatus {
-    UNASSIGNED("unassigned"),
-    ASSIGNED_PENDING("open"),
-    ACCEPTED("accepted"),
-    COMPLETED("complete"),
-    CANCELED("canceled");
-
-    final String status;
-
-    OrderStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
+    UNASSIGNED,
+    ASSIGNED,
+    ACCEPTED,
+    COMPLETED,
+    CANCELED
 }
