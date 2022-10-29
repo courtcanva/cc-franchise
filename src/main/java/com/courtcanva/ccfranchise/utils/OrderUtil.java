509b59cc3740f0873dfa3ddb75938bd5fa486374
Gordon <sgz886@gmail.com>
2022-10-29 22:35:28 +1100

    refine: refine code based on code review

--- src/main/java/com/courtcanva/ccfranchise/utils/OrderUtil.java
+++ src/main/java/com/courtcanva/ccfranchise/utils/OrderUtil.java
@@ -3,6 +3,10 @@ package com.courtcanva.ccfranchise.utils;
 import com.courtcanva.ccfranchise.constants.OrderStatus;
 
 public class OrderUtil {
+
+    private OrderUtil() {
+    }
+
     public static boolean statusValid(String test) {
         for (OrderStatus orderStatus : OrderStatus.values()) {
             if (orderStatus.value.equals(test)) {
