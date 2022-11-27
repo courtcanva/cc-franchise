7e0c1e31b4794263000a0a2221d8bc9f5233a184
Prom1s1ngYoung <yqin7516@gmail.com>
2022-11-27 19:21:11 +1100

    update test

--- src/test/java/com/courtcanva/ccfranchise/services/OrderServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.courtcanva.ccfranchise.services;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class OrderServiceTest {
-}
