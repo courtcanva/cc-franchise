38b97ac55d4be6d97edbd6f5bd6f2e873766cb49
Iris99999 <753891368@qq.com>
2022-11-28 11:29:33 +0800

    change order status

--- src/main/java/com/courtcanva/ccfranchise/services/FranchiseeService.java
+++ src/main/java/com/courtcanva/ccfranchise/services/FranchiseeService.java
@@ -185,7 +185,6 @@ public class FranchiseeService {
                         .collect(Collectors.toList())).build();
     }
 
-
     public List<Franchisee> findMatchedFranchisee(long sscCode, long orderId) {
 
         Set<Suburb> dutyAreas = new HashSet<>(suburbService.findSuburbBySscCodes(List.of(sscCode)));
